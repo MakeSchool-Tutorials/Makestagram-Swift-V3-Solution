86d744ea800b37b4797e608c944100c036c8b97d
Dion Larson <me@dionlarson.com>
2018-07-08 12:05:45 -0700

    Setup tab bar controller, through page 11

---
+++ Makestagram/Controllers/FindFriendsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FindFriendsViewController.swift
+//  Makestagram
+//
+//  Created by Dion Larson on 7/8/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import UIKit
+
+class FindFriendsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ Makestagram/Controllers/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  Makestagram
+//
+//  Created by Dion Larson on 7/8/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
