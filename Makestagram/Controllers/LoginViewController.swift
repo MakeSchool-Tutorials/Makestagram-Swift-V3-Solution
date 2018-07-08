d434ebd00221263737eb98142a52e1382934a99f
Dion Larson <me@dionlarson.com>
2018-07-08 08:58:52 -0700

    Add Login storyboard, through page 6

---
+++ Makestagram/Controllers/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Dion Larson on 7/8/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseAuthUI
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        print("Login button tapped")
+    }
+    
+}
