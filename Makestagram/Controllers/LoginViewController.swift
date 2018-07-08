1766e3b5284f073da801f6be2a91f9e2a7fa0829
Dion Larson <me@dionlarson.com>
2018-07-08 09:29:50 -0700

    Add auth/signup code, through page 7

--- Makestagram/Controllers/LoginViewController.swift
+++ Makestagram/Controllers/LoginViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 import FirebaseAuth
 import FirebaseAuthUI
 
+typealias FIRUser = FirebaseAuth.User
+
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var loginButton: UIButton!
@@ -37,7 +39,26 @@ class LoginViewController: UIViewController {
     */
 
     @IBAction func loginButtonTapped(_ sender: Any) {
-        print("Login button tapped")
+        // 1
+        guard let authUI = FUIAuth.defaultAuthUI()
+            else { return }
+        
+        // 2
+        authUI.delegate = self
+        
+        // 3
+        let authViewController = authUI.authViewController()
+        present(authViewController, animated: true)
     }
-    
+}
+
+extension LoginViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith user: FIRUser?, error: Error?) {
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        
+        print("handle user signup / login")
+    }
 }
